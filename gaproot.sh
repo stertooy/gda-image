e1c7a3561a063a7943baff6a01adf0463d82d561
stertooy <5571903+stertooy@users.noreply.github.com>
2025-12-18 18:38:50 +0100

    Delete gaproot.sh

--- gaproot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Only run once per container
-if [ -z "${GAPROOT_SYMLINKED:-}" ]; then
-  export GAPROOT_SYMLINKED=1
-  BLAH="$PWD"
-  echo $BLAH > /test.txt
-  ln -sf "$PWD" /tmp/gaproot/pkg/
-fi
